0c8f90ee9da607026d3212e292862640205f949b
Guill Lo <guill.bout@gmail.com>
2021-10-26 18:22:45 +0800

    first commit

---
+++ Monterey-AX-UITests-apiDisabled/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Monterey-AX-UITests-apiDisabled
+//
+//  Created by Guillaume Leclerc on 26/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Monterey-AX-UITests-apiDisabled/Monterey_AX_UITests_apiDisabledApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monterey_AX_UITests_apiDisabledApp.swift
+//  Monterey-AX-UITests-apiDisabled
+//
+//  Created by Guillaume Leclerc on 26/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Monterey_AX_UITests_apiDisabledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
